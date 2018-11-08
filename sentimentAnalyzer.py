7afaeafbaf3afecff25fa2e0079ce03b2e8454e6
sorathesy <cesardedios94@gmail.com>
2018-11-07 21:22:31 -0500

    Removed api keys, fully functional, great little script, will refactor

--- sentimentAnalyzer.py
+++ sentimentAnalyzer.py
@@ -6,29 +6,29 @@
 #wiki.words
 #wiki.sentiment.polarity
 
-import tweetpy
+import tweepy
 from textblob import TextBlob as TB 
 
 # Api keys to connect to the twitter API
-consumer_key = 'ElOVheQZ5EixJ92d8spajZ7QT'
-consumer_secret = '6MWE1vKpAlpiNK96VG4xeqyJWxfvXnNideCAYZNDVx6iN9Hthz'
+consumer_key = 'xxxxx'
+consumer_secret = 'xxxxx'
 
 # access tokens to gain rw access through authentication
-access_token = '2895786628-TyGpy0Ffrs6uhfOgOyuMtbHmsKxFfuO12tGP51Y'
-access_token_secret = 'py2ql1fWqnSJjGSTr6h09ytbeKGJcd5BK7ihWySG46RoP'
+access_token = 'xxxx'
+access_token_secret = 'xxx'
 
 # Auth variable to handle API handshake and Auth access with twitter
-auth = tweetpy.OAuthHandler(consumer_key, consumer_secret)
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 
 # API connection object
-api = tweetpy.API(auth)
+api = tweepy.API(auth)
 
 # variable to search for tweets that contain a specific term
-public_tweets = api.search('feet')
+public_tweets = api.search('Pokemon')
 
 for tweet in public_tweets:
     print(tweet.text)
-    analysis = TextBlob(tweet.text)
+    analysis = TB(tweet.text)
     print(analysis.sentiment)
 
