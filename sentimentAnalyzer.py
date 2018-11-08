92fa4906f5e89aefc6a37f22ae7e12e2d9071f0a
sorathesy <cesardedios94@gmail.com>
2018-11-07 21:53:34 -0500

    Writes tweets to csv with sentiments and tweet in different columns

--- sentimentAnalyzer.py
+++ sentimentAnalyzer.py
@@ -8,6 +8,8 @@
 
 import tweepy
 from textblob import TextBlob as TB 
+import csv
+
 
 # Api keys to connect to the twitter API
 consumer_key = 'xxxxx'
@@ -21,14 +23,24 @@ access_token_secret = 'xxx'
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 
-# API connection object
-api = tweepy.API(auth)
+# API connection object with limit rate 
+api = tweepy.API(auth, wait_on_rate_limit=True)
 
 # variable to search for tweets that contain a specific term
 public_tweets = api.search('Pokemon')
 
-for tweet in public_tweets:
-    print(tweet.text)
-    analysis = TB(tweet.text)
-    print(analysis.sentiment)
 
+
+#for tweet in public_tweets:
+    #print(tweet.text)
+    #analysis = TB(tweet.text)
+    #print(analysis.sentiment)
+
+with open("sentiment.csv",'w', encoding = 'utf-8-sig', newline='') as tf:
+    writer = csv.writer(tf)
+
+    for tweet in public_tweets:
+        print(tweet.text)
+        analysis = TB(tweet.text)
+        print(analysis.sentiment)
+        writer.writerow([tweet.text,analysis.sentiment])
